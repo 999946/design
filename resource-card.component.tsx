9848d437f396e3f28fb66265fb332fd377f2b7e1
huangziyi01 <huangziyi01@baidu.com>
2016-08-25 18:30:42 +0800

    quick push

---
+++ resource-card.component.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import * as typings from './resource-card.type'
+import style from './resource-card.style'
+import {View, Image, Text} from 'react-native'
+
+export default class ResourceCard extends React.Component <typings.PropsDefine, typings.StateDefine> {
+    static defaultProps: typings.PropsDefine = new typings.Props()
+    public state: typings.StateDefine = new typings.State()
+
+    render() {
+        return (
+            <View style={style.content}>
+                <Image style={style.image}
+                       source={this.props.pictureSource}/>
+                <View style={style.textContainer}>
+                    <Text style={style.text}
+                          numberOfLines={3}>{this.props.title}</Text>
+                </View>
+            </View>
+        )
+    }
+}
