3f1577cdaf93980ce5d2e10a4ff26700a76b4207
huangziyi01 <huangziyi01@baidu.com>
2016-08-26 17:12:34 +0800

    微粉导航条新增title默认值

--- scripts/manage/index.ts
+++ scripts/manage/index.ts
@@ -1,11 +1,13 @@
 import updater from './updater'
 import pusher from './pusher'
+import publisher from './publisher'
 
 const commander = require('commander')
 
 commander.version('1.0.0')
     .option('-u, --update', '更新')
     .option('-p, --push', '提交')
+    .option('-p, --publish', '发布')
     .option('-t, --travis', '持续集成')
     .option('-tk, --token', 'github的token')
     .option('-m, --message [value]', '提交时带的信息', 'quick push')
@@ -17,4 +19,8 @@ if (commander['update']) {
 
 if (commander['push']) {
     pusher(commander)
+}
+
+if (commander['publish']){
+
 }
---
+++ scripts/manage/publisher.ts
@@ -0,0 +1,17 @@
+/**
+ * 发布
+ */
+
+import * as config from '../../config'
+import components from '../../components'
+import {exec, execSync} from 'child_process'
+import * as path from 'path'
+import * as fs from 'fs'
+import hasChange from './utils/has-change'
+import consoleLog from './utils/console-log'
+
+export default (commander: any)=> {
+
+
+
+}
