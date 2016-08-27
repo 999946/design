67b528ee77488763c8bb54d126205162e85b5167
huangziyi01 <huangziyi01@baidu.com>
2016-08-27 11:23:58 +0800

    修改编译脚本

--- scripts/manage/utils/html-css-path-parse.ts
+++ scripts/manage/utils/html-css-path-parse.ts
@@ -1,3 +1,6 @@
 export default (filePath: string, component: Components.ComponentConfig, category: Components.Category)=> {
+    // 把路径前缀去掉
+    filePath = filePath.replace(`components/${category.name}/${component.name}/lib`, '')
+
     return filePath
 }
