5c937dfebf19910159d89c29cfabd56f27de393a
huangziyi01 <huangziyi01@baidu.com>
2016-08-27 11:28:42 +0800

    修改编译脚本

--- scripts/manage/utils/html-css-path-parse.ts
+++ scripts/manage/utils/html-css-path-parse.ts
@@ -1,6 +1,14 @@
 export default (filePath: string, component: Components.ComponentConfig, category: Components.Category)=> {
     // 把路径前缀去掉
-    filePath = filePath.replace(`components/${category.name}/${component.name}/lib`, '')
+    filePath = filePath.replace(`components/${category.name}/${component.name}/lib/`, '')
+
+    // 返回的前缀
+    const prefix = `${category.prefix}-${component.name}`
+
+    if (filePath.indexOf('/')) {
+        // 如果没有 /, 说明是第一级
+        return prefix
+    }
 
     return filePath
 }
