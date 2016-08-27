9ea93958564d0d2b802187a7e447a889acd7f5c2
huangziyi01 <huangziyi01@baidu.com>
2016-08-27 11:45:30 +0800

    修改编译脚本

--- scripts/manage/utils/html-css-path-parse.ts
+++ scripts/manage/utils/html-css-path-parse.ts
@@ -5,13 +5,10 @@ export default (filePath: string, component: Components.ComponentConfig, categor
     // 返回的前缀
     const prefix = `${category.prefix}-${component.name}`
 
-    console.log(filePath)
     if (filePath.indexOf('/') === -1) {
-        console.log('是第一级')
         // 如果没有 /, 说明是第一级
         return prefix
     } else {
-        console.log('不是第一级')
         filePath = filePath.replace(/-/g, '_')
         filePath = filePath.replace(/\//g, '-')
     }
