75d65b6387b04583d17251b4b665d924c0373271
huangziyi01 <huangziyi01@baidu.com>
2016-08-29 09:52:33 +0800

    根目录居然过滤了lib目录，修正

---
+++ components/wefan/navbar/lib/navbar.style.d.ts
@@ -0,0 +1,14 @@
+declare var _default: {
+    container: React.ViewStyle;
+    navBtn: React.ViewStyle;
+    navBtnRight: React.ViewStyle;
+    titleText: {
+        fontSize: number;
+        color: string;
+        fontWeight: string;
+    };
+    centerContainer: React.ViewStyle;
+    leftContainer: React.ViewStyle;
+    rightContainer: React.ViewStyle;
+};
+export default _default;
