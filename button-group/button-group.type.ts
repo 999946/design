7a1f4910fd52e82e837f676bfd6342ecea96a376
huangziyi01 <huangziyi01@baidu.com>
2016-09-05 13:09:13 +0800

    新增 nt-button，优化编译脚本

---
+++ button-group/button-group.type.ts
@@ -0,0 +1,26 @@
+import * as React from 'react'
+
+export interface PropsDefine extends React.HTMLAttributes{
+    /**
+     * 是垂直显示
+     */
+    vertical?: boolean
+}
+
+export class PropsGaea {
+    gaeaName = '按钮组'
+    gaeaIcon = 'square-o'
+    gaeaUniqueKey = 'common-button-group'
+}
+
+export class Props extends PropsGaea implements PropsDefine {
+    vertical = false
+}
+
+export interface StateDefine {
+
+}
+
+export class State implements StateDefine {
+
+}
