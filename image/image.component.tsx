2dbbb076f06dc767f2e292adab53e07124a7abaf
huangziyi01 <huangziyi01@baidu.com>
2016-09-07 15:39:56 +0800

    完善图片组件

--- image/image.component.tsx
+++ image/image.component.tsx
@@ -1,15 +1,64 @@
 import * as React from 'react'
 import * as typings from './image.type'
+import {TouchableOpacity, Image} from 'react-native'
 
-export default class Image extends React.Component <typings.PropsDefine, typings.StateDefine> {
+export default class ImageComponent extends React.Component <typings.PropsDefine, typings.StateDefine> {
     static defaultProps: typings.PropsDefine = new typings.Props()
     public state: typings.StateDefine = new typings.State()
 
+    // 兜底图加载失败次数， 失败 3 次会取消一切补救操作
+    private fallbackLoadCounter = 0
+
+    // 是否成功加载了预期图片
+    private loadImageSuccess = false
+
+    componentWillMount() {
+        this.setState({
+            source: this.props.source
+        })
+    }
+
+    handleLoadSuccess() {
+        this.fallbackLoadCounter = 0
+
+        if (this.state.source === this.props.source) {
+            this.loadImageSuccess = true
+        }
+    }
+
+    handleLoadError() {
+        this.loadImageSuccess = false
+        if (this.props.fallbackSource) {
+            if (this.fallbackLoadCounter < 3) {
+                this.fallbackLoadCounter++
+                // 如果设置了兜底图，现在展示它
+                this.setState({
+                    source: this.props.fallbackSource
+                })
+            }
+        }
+    }
+
+    handlePress() {
+        this.props.onPress && this.props.onPress()
+        if (this.props.pressToReload && this.loadImageSuccess === false) {
+            // 重新加载原图
+            this.fallbackLoadCounter = 0
+            this.setState({
+                source: this.props.source
+            })
+        }
+    }
+
     render() {
         return (
-            <div>
-                图片
-            </div>
+            <TouchableOpacity activeOpacity={1}
+                              onPress={this.handlePress.bind(this)}>
+                <Image style={this.props.style}
+                       onError={this.handleLoadError.bind(this)}
+                       onLoad={this.handleLoadSuccess.bind(this)}
+                       source={this.state.source}/>
+            </TouchableOpacity>
         )
     }
 }
--- image/image.type.ts
+++ image/image.type.ts
@@ -1,7 +1,74 @@
 import * as React from 'react'
+import * as ReactNative from 'react-native'
 
-export interface PropsDefine extends React.HTMLAttributes {
+export interface PropsDefine extends ReactNative.ImageProperties {
+    /**
+     * 加载失败时的图片地址
+     */
+    fallbackSource?: {uri: string} | string
 
+    /**
+     * 当属性为真的时候， 点击图片会重新尝试拉取原图片
+     */
+    pressToReload?: boolean
+
+    /**
+     * 当点击图片时触发
+     */
+    onPress?: ()=>void
+
+    /**
+     * onLayout function
+     *
+     * Invoked on mount and layout changes with
+     *
+     * {nativeEvent: { layout: {x, y, width, height}}}.
+     */
+    onLayout?: (event: ReactNative.LayoutChangeEvent) => void
+
+    /**
+     * Invoked when load completes successfully
+     */
+    onLoad?: () => void
+
+    /**
+     * Invoked when load either succeeds or fails
+     */
+    onLoadEnd?: () => void
+
+    /**
+     * Invoked on load start
+     */
+    onLoadStart?: () => void
+
+
+    /**
+     * Determines how to resize the image when the frame
+     * doesn't match the raw image dimensions.
+     *
+     * enum('cover', 'contain', 'stretch')
+     */
+    resizeMode?: "cover" | "contain" | "stretch"
+
+    /**
+     * uri is a string representing the resource
+     * identifier for the image,
+     * which could be an http address, a local file path,
+     * or the name of a static image resource (which
+     * should be wrapped in the require('image!name') function).
+     */
+    source: {uri: string} | string
+
+    /**
+     * Style
+     */
+    style?: ReactNative.ImageStyle
+
+    /**
+     * A unique identifier for this element to be used
+     * in UI Automation testing scripts.
+     */
+    testID?: string
 }
 
 export class PropsGaea {
@@ -11,14 +78,15 @@ export class PropsGaea {
 }
 
 export class Props extends PropsGaea implements PropsDefine {
-    vertical = false
+    source = ''
+    pressToReload = false
 }
 
 export interface StateDefine {
-
+    source: {uri: string} | string
 }
 
 export class State implements StateDefine {
-
+    source = ''
 }
                 
