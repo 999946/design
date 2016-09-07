8941ba29258c9cdd4569299e3d02f0d15222e171
huangziyi01 <huangziyi01@baidu.com>
2016-09-07 16:31:35 +0800

    完善gif

--- demo/click-to-play.tsx
+++ demo/click-to-play.tsx
@@ -4,14 +4,14 @@ import Gif from '../index'
 
 @observer
 export default class Demo extends React.Component <any, any> {
-    static title = '基本用法'
+    static title = '点击后播放'
     static description = ``
 
     render() {
         return (
-            <Gif source={{uri:'http://img1.imgtn.bdimg.com/it/u=1452441092,486151961&fm=21&gp=0.jpg'}}
+            <Gif firstSource={require('./images/first.png')}
+                 source={require('./images/all.gif')}
                  style={{width:200,height:150}}/>
         )
     }
 }
-                
--- gif/gif.component.tsx
+++ gif/gif.component.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react'
 import * as typings from './gif.type'
+import Image from '../../image/index'
+import {View} from 'react-native'
 
 export default class Gif extends React.Component <typings.PropsDefine, typings.StateDefine> {
     static defaultProps: typings.PropsDefine = new typings.Props()
     public state: typings.StateDefine = new typings.State()
 
+    componentWillMount() {
+        this.setState({
+            source: this.props.firstSource || this.props.source
+        })
+    }
+
+    handlePress() {
+        this.props.onPress && this.props.onPress()
+
+        this.setState({
+            source: this.props.source
+        })
+    }
+
     render() {
         return (
-            <div>
-                GIF
-            </div>
+            <View>
+                <Image source={this.state.source}
+                       style={this.props.style}
+                       onPress={this.handlePress.bind(this)}/>
+            </View>
         )
     }
 }
