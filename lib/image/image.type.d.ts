995e0d8c712b32659caa941f2bf8b325deb18e42
huangziyi01 <huangziyi01@baidu.com>
2016-09-08 09:17:37 +0800

    开发

--- lib/image/image.type.d.ts
+++ lib/image/image.type.d.ts
@@ -1,10 +1,15 @@
+import * as React from 'react';
 import * as ReactNative from 'react-native';
 export interface PropsDefine extends ReactNative.ImageProperties {
     fallbackSource?: {
         uri: string;
     } | string;
+    fallbackColor?: string;
     pressToReload?: boolean;
     onPress?: () => void;
+    fallbackAddon?: () => React.ReactElement<any>;
+    fallbackHideImage?: boolean;
+    onError?: () => void;
     onLayout?: (event: ReactNative.LayoutChangeEvent) => void;
     onLoad?: () => void;
     onLoadEnd?: () => void;
@@ -15,6 +20,7 @@ export interface PropsDefine extends ReactNative.ImageProperties {
     } | string;
     style?: ReactNative.ImageStyle;
     testID?: string;
+    others?: any;
 }
 export declare class PropsGaea {
     gaeaName: string;
@@ -24,12 +30,18 @@ export declare class PropsGaea {
 export declare class Props extends PropsGaea implements PropsDefine {
     source: string;
     pressToReload: boolean;
+    onPress: () => void;
+    fallbackAddon: () => any;
+    onError: () => void;
+    fallbackHideImage: boolean;
 }
 export interface StateDefine {
-    source: {
+    source?: {
         uri: string;
     } | string;
+    loadImageSuccess?: boolean;
 }
 export declare class State implements StateDefine {
     source: string;
+    loadImageSuccess: boolean;
 }
