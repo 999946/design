40b8cca7380f10d8d619f89b01fb4da09cc1f9bb
huangziyi01 <huangziyi01@baidu.com>
2016-09-09 16:46:51 +0800

    开发

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import Gif from './gif/gif.component'
+import {PropsDefine as GifPropsDefine} from './gif/gif.type'
 
-export {Gif}
+export {Gif, GifPropsDefine}
 export default Gif
                 
