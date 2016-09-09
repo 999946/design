4894f78ed4d89143116d1ff8bd9baf74bdf26748
huangziyi01 <huangziyi01@baidu.com>
2016-09-09 17:37:30 +0800

    开发

--- scripts/manage/builder.ts
+++ scripts/manage/builder.ts
@@ -112,13 +112,14 @@ const parseDts = (filePath: string, component: Components.ComponentConfig, categ
 
             if (`${importFullPathSplit[1]}/${importFullPathSplit[2]}` !== `${category.name}/${component.name}`) {
                 // 保证引用的模块不是自己
-                console.log(`import ${match[1]} ${match[2]} '${category.prefix}-${component.name}'`, match)
-                return `import ${match[1]} ${match[2]} '${category.prefix}-${component.name}'`
+                return `import ${match[1]} '${category.prefix}-${component.name}'`
             }
         }
 
         return match[0]
     })
+
+    fs.writeFileSync(cssPath, postResult)
 }
 
 /**
