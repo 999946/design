74346840c901e21ebe506259f8cae1f86ef7fdc4
huangziyi01 <huangziyi01@baidu.com>
2016-09-09 21:57:50 +0800

    改了太多东西

--- image/image.type.ts
+++ image/image.type.ts
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import * as ReactNative from 'react-native'
+import {ImageProperties, LayoutChangeEvent, ImageStyle} from 'react-native'
 
-export interface PropsDefine extends ReactNative.ImageProperties {
+export interface PropsDefine extends ImageProperties {
     /**
      * 加载失败时的图片地址
      */
@@ -36,59 +36,6 @@ export interface PropsDefine extends ReactNative.ImageProperties {
      * 加载图片失败时的回调
      */
     onError?: ()=>void
-
-    /**
-     * onLayout function
-     *
-     * Invoked on mount and layout changes with
-     *
-     * {nativeEvent: { layout: {x, y, width, height}}}.
-     */
-    onLayout?: (event: ReactNative.LayoutChangeEvent) => void
-
-    /**
-     * Invoked when load completes successfully
-     */
-    onLoad?: () => void
-
-    /**
-     * Invoked when load either succeeds or fails
-     */
-    onLoadEnd?: () => void
-
-    /**
-     * Invoked on load start
-     */
-    onLoadStart?: () => void
-
-
-    /**
-     * Determines how to resize the image when the frame
-     * doesn't match the raw image dimensions.
-     *
-     * enum('cover', 'contain', 'stretch')
-     */
-    resizeMode?: "cover" | "contain" | "stretch"
-
-    /**
-     * uri is a string representing the resource
-     * identifier for the image,
-     * which could be an http address, a local file path,
-     * or the name of a static image resource (which
-     * should be wrapped in the require('image!name') function).
-     */
-    source: {uri: string} | string
-
-    /**
-     * Style
-     */
-    style?: ReactNative.ImageStyle
-
-    /**
-     * A unique identifier for this element to be used
-     * in UI Automation testing scripts.
-     */
-    testID?: string
 
     others?: any
 }
