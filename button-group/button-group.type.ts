766444e784cfdf668d90f28399ebfa9ae9c16994
huangziyi01 <huangziyi01@baidu.com>
2016-09-13 22:43:15 +0800

    调整编译脚本 新增 image-zoom image-viewer

--- button-group/button-group.type.ts
+++ button-group/button-group.type.ts
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export interface PropsDefine extends React.HTMLAttributes{
+export interface PropsDefine extends React.HTMLAttributes {
     /**
      * 是垂直显示
      */
