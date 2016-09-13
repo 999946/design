096f383316b341a1bd17c2ea1b84cbdc0771cbe8
huangziyi01 <huangziyi01@baidu.com>
2016-09-13 22:43:15 +0800

    调整编译脚本 新增 image-zoom image-viewer

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,5 +1,5 @@
 "use strict";
 const image_component_1 = require('./image/image.component');
 exports.Image = image_component_1.default;
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 exports.default = image_component_1.default;
