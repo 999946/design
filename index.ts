1fadb3ec7fe366733f4fc4fc1c3f9dfeb107d3d6
huangziyi01 <huangziyi01@baidu.com>
2016-09-14 18:56:02 +0800

    新增 margin-padding-editor

---
+++ index.ts
@@ -0,0 +1,6 @@
+import MarginPaddingEditor from './margin-padding-editor/margin-padding-editor.component'
+import {PropsDefine as MarginPaddingEditorPropsDefine} from './margin-padding-editor/margin-padding-editor.type'
+
+export {MarginPaddingEditor, MarginPaddingEditorPropsDefine}
+export default MarginPaddingEditor
+                
