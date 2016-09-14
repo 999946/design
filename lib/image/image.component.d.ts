33fd947ba28a998e84fc6fabfe80ab71e4341de6
huangziyi01 <huangziyi01@baidu.com>
2016-09-14 15:05:13 +0800

    发布组件

--- lib/image/image.component.d.ts
+++ lib/image/image.component.d.ts
@@ -4,16 +4,10 @@ export default class ImageComponent extends React.Component<typings.PropsDefine,
     static defaultProps: typings.PropsDefine;
     state: typings.StateDefine;
     private fallbackLoadCounter;
-
     componentWillMount(): void;
-
     componentWillReceiveProps(nextProps: typings.PropsDefine): void;
-
     handleLoadSuccess(): void;
-
     handleLoadError(): void;
-
     handlePress(): void;
-
     render(): JSX.Element;
 }
