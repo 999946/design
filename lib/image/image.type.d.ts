b2244e5b17e93696cded4f6bf6fa8db9f2534e89
huangziyi01 <huangziyi01@baidu.com>
2016-09-14 15:05:13 +0800

    发布组件

--- lib/image/image.type.d.ts
+++ lib/image/image.type.d.ts
@@ -1,26 +1,5 @@
-import * as React from 'react';
-import * as ReactNative from 'react-native';
-export interface PropsDefine extends ReactNative.ViewProperties {
-    source: {
-        uri: string;
-    } | string;
-    firstSource?: {
-        uri: string;
-    } | string;
-    fallbackSource?: {
-        uri: string;
-    } | string;
-    fallbackColor?: string;
-    fallbackAddon?: () => React.ReactElement<any>;
-    pressToReload?: boolean;
-    onPress?: () => void;
-    onLayout?: (event: ReactNative.LayoutChangeEvent) => void;
-    onLoad?: () => void;
-    onLoadEnd?: () => void;
-    onLoadStart?: () => void;
-    resizeMode?: "cover" | "contain" | "stretch";
-    style?: ReactNative.ImageStyle;
-    testID?: string;
+import { GifPropsDefine } from  'nt-gif';
+export interface PropsDefine extends GifPropsDefine {
     others?: any;
 }
 export declare class PropsGaea {
