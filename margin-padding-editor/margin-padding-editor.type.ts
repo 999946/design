fa989340419f03d6af7810a920e648a211917837
huangziyi01 <huangziyi01@baidu.com>
2016-09-16 20:37:09 +0800

    更新

--- margin-padding-editor/margin-padding-editor.type.ts
+++ margin-padding-editor/margin-padding-editor.type.ts
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import * as ReactNative from 'react-native'
 
 export type MarginPaddingField = 'paddingLeft' | 'paddingTop' | 'paddingRight' | 'paddingBottom' | 'marginLeft' | 'marginTop' | 'marginRight' | 'marginBottom' | ''
 
