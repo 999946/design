6836fab49cb656aaf53d9832511fd175889fb33c
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 20:59:02 +0800

    add radio checkbox collapse

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import Timeago from './timeago/timeago.component';
-import { PropsDefine as TimeagoPropsDefine } from './timeago/timeago.type';
-export { Timeago, TimeagoPropsDefine };
+import {PropsDefine as TimeagoPropsDefine} from './timeago/timeago.type';
+export {Timeago, TimeagoPropsDefine};
 export default Timeago;
