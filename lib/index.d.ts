64143b95a52d66917d834aeb3f5e3e7f79d2dd3e
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 11:07:40 +0800

    发布组件

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import Timeago from './timeago/timeago.component';
+import { PropsDefine as TimeagoPropsDefine } from './timeago/timeago.type';
+export { Timeago, TimeagoPropsDefine };
+export default Timeago;
---
+++ lib/timeago/timeago.component.d.ts
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as typings from './timeago.type';
+export default class Timeago extends React.Component<typings.PropsDefine, typings.StateDefine> {
+    static defaultProps: typings.PropsDefine;
+    state: typings.StateDefine;
+    private _isMounted;
+    private timeoutId;
+    constructor(props: any);
+    protected componentWillMount(): void;
+    protected componentDidMount(): void;
+    protected componentDidUpdate(nextProps: typings.PropsDefine): void;
+    protected componentWillUnmount(): void;
+    protected tick(refresh?: boolean): void;
+    render(): React.ReactElement<any>;
+}
