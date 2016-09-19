3167520c077cdbf00a155154addc6775a4c4db23
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 20:59:02 +0800

    add radio checkbox collapse

--- lib/switch/switch.component.d.ts
+++ lib/switch/switch.component.d.ts
@@ -4,8 +4,12 @@ import './switch.scss';
 export default class Switch extends React.Component<typings.PropsDefine, typings.StateDefine> {
     static defaultProps: typings.PropsDefine;
     state: typings.StateDefine;
+
     componentWillMount(): void;
+
     componentWillReceiveProps(nextProps: typings.PropsDefine): void;
+
     toggle(): void;
+
     render(): JSX.Element;
 }
