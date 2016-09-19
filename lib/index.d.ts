ef144ae20ed1d19331cde8ccb987fbc7723c1e14
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 21:44:19 +0800

    开发

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Checkbox from './checkbox/checkbox.component';
-import { PropsDefine as CheckboxPropsDefine } from './checkbox/checkbox.type';
-export { Checkbox, CheckboxPropsDefine };
-export default Checkbox;
