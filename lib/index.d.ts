7e7206c3efeb2f769ce5f3e393d04cc88f1579e5
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 20:59:02 +0800

    add radio checkbox collapse

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import ImagePreload from './image-preload/image-preload.component';
-import { PropsDefine as ImagePreloadPropsDefine } from './image-preload/image-preload.type';
-export { ImagePreload, ImagePreloadPropsDefine };
+import {PropsDefine as ImagePreloadPropsDefine} from './image-preload/image-preload.type';
+export {ImagePreload, ImagePreloadPropsDefine};
 export default ImagePreload;
