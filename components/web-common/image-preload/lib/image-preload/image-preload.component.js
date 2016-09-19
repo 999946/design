3136e0aa76e1b535fee9450619f5ab827e68507b
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 09:43:46 +0800

    发布组件

---
+++ components/web-common/image-preload/lib/image-preload/image-preload.component.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = function (pictureUrls, callback) {
+    var loaded = 0;
+    for (var i = 0, j = pictureUrls.length; i < j; i++) {
+        (function (img, src) {
+            img.onload = function () {
+                if (++loaded == pictureUrls.length && callback) {
+                    callback();
+                }
+            };
+            img.src = src;
+        })(new Image(), pictureUrls[i]);
+    }
+};
---
+++ components/web-common/image-preload/lib/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var image_preload_component_1 = require('./image-preload/image-preload.component');
+exports.ImagePreload = image_preload_component_1.default;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = image_preload_component_1.default;
