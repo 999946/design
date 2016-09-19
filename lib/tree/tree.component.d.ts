e7dbddd89b6b8591d436b7e980778026d4659d5d
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 20:59:02 +0800

    add radio checkbox collapse

--- lib/tree/tree.component.d.ts
+++ lib/tree/tree.component.d.ts
@@ -3,5 +3,6 @@ import * as typings from './tree.type';
 export default class Tree extends React.Component<typings.PropsDefine, typings.StateDefine> {
     static defaultProps: typings.PropsDefine;
     state: typings.StateDefine;
+
     render(): JSX.Element;
 }
