875d032831c0c5d8dd8384568c91611725f5e05c
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 15:01:54 +0800

    发布组件

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import Input from './input/input.component';
+import { PropsDefine as InputPropsDefine } from './input/input.type';
+export { Input, InputPropsDefine };
+export { ExtendValidatorStatic } from './input/validate';
+export default Input;
