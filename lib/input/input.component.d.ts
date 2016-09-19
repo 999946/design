0dea37f2cbb6152215d0d420922df9c50c475a34
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 20:59:02 +0800

    add radio checkbox collapse

--- lib/input/input.component.d.ts
+++ lib/input/input.component.d.ts
@@ -4,7 +4,10 @@ import './input.scss';
 export default class Input extends React.Component<typings.PropsDefine, typings.StateDefine> {
     static defaultProps: typings.PropsDefine;
     state: typings.StateDefine;
+
     componentWillMount(): void;
+
     handleInputChange(event: any): void;
+
     render(): JSX.Element;
 }
