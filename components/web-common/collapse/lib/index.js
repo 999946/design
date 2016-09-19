ec95e5f4fe45391f709543606da910636128d282
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 21:46:38 +0800

    发布组件

---
+++ components/web-common/collapse/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var collapse_component_1 = require('./collapse/collapse.component');
+exports.Collapse = collapse_component_1.default;
+var coll_panel_component_1 = require('./coll-panel/coll-panel.component');
+exports.CollPanel = coll_panel_component_1.default;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = collapse_component_1.default;
