b5a06b9b48ab77b83fbfc9a74b2b065cc9fecb62
huangziyi01 <huangziyi01@baidu.com>
2016-09-19 09:54:24 +0800

    发布组件

---
+++ lib/loading/loading.type.d.ts
@@ -0,0 +1,16 @@
+import { HTMLAttributes } from 'react';
+export interface PropsDefine extends HTMLAttributes {
+    size?: number;
+    others?: any;
+}
+export declare class PropsGaea {
+    gaeaName: string;
+    gaeaIcon: string;
+    gaeaUniqueKey: string;
+}
+export declare class Props extends PropsGaea implements PropsDefine {
+}
+export interface StateDefine {
+}
+export declare class State implements StateDefine {
+}
