25cbf510c936b6d07fff30daef94f7732511e682
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 15:45:26 +0800

    add tabs

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import Checkbox from './checkbox/checkbox.component';
-import { PropsDefine as CheckboxPropsDefine } from './checkbox/checkbox.type';
-export { Checkbox, CheckboxPropsDefine };
+import {PropsDefine as CheckboxPropsDefine} from './checkbox/checkbox.type';
+export {Checkbox, CheckboxPropsDefine};
 export default Checkbox;
