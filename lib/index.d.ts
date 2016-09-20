af61adca71d523383c3f744cd9f417ce6a244eee
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:38:46 +0800

    开发

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import Message from './message/message.component';
-import {PropsDefine as MessagePropsDefine} from './message/message.type';
-export {Message, MessagePropsDefine};
+import { PropsDefine as MessagePropsDefine } from './message/message.type';
+export { Message, MessagePropsDefine };
 export default Message;
