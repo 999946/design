e3508f9e28909ccbc9a226bdfc4f23f5071beddd
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:38:46 +0800

    开发

--- lib/timeago/timeago.component.d.ts
+++ lib/timeago/timeago.component.d.ts
@@ -5,18 +5,11 @@ export default class Timeago extends React.Component<typings.PropsDefine, typing
     state: typings.StateDefine;
     private _isMounted;
     private timeoutId;
-
     constructor(props: any);
-
     protected componentWillMount(): void;
-
     protected componentDidMount(): void;
-
     protected componentDidUpdate(nextProps: typings.PropsDefine): void;
-
     protected componentWillUnmount(): void;
-
     protected tick(refresh?: boolean): void;
-
     render(): React.ReactElement<any>;
 }
