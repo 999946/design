0a1e88c5ac9d0635fdc3f397fe06e50c0986d54b
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:38:46 +0800

    开发

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import Switch from './switch/switch.component';
-import {PropsDefine as SwitchPropsDefine} from './switch/switch.type';
-export {Switch, SwitchPropsDefine};
+import { PropsDefine as SwitchPropsDefine } from './switch/switch.type';
+export { Switch, SwitchPropsDefine };
 export default Switch;
