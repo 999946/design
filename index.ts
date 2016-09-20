9365ea20b442af9dc2ca177f2b8d49aa64d1948e
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 15:45:26 +0800

    add tabs

---
+++ index.ts
@@ -0,0 +1,6 @@
+import JsonTree from './json-tree/json-tree.component'
+import {PropsDefine as JsonTreePropsDefine} from './json-tree/json-tree.type'
+
+export {JsonTree, JsonTreePropsDefine}
+export default JsonTree
+                
---
+++ json-tree/json-tree.type.ts
@@ -0,0 +1,24 @@
+import * as React from 'react'
+
+export interface PropsDefine {
+
+}
+
+export class PropsGaea {
+    gaeaName = 'json 树'
+    gaeaIcon = 'square-o'
+    gaeaUniqueKey = 'nt-web-json-tree'
+}
+
+export class Props extends PropsGaea implements PropsDefine {
+
+}
+
+export interface StateDefine {
+
+}
+
+export class State implements StateDefine {
+
+}
+                
