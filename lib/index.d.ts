62d7f5422bd8a90aa77f87eb8384ac71583d5635
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:38:46 +0800

    开发

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import Button from './button/button.component';
-import {PropsDefine as ButtonPropsDefine} from './button/button.type';
+import { PropsDefine as ButtonPropsDefine } from './button/button.type';
 import ButtonGroup from './button-group/button-group.component';
-import {PropsDefine as ButtonGroupPropsDefine} from './button-group/button-group.type';
-export {Button, ButtonPropsDefine, ButtonGroup, ButtonGroupPropsDefine};
+import { PropsDefine as ButtonGroupPropsDefine } from './button-group/button-group.type';
+export { Button, ButtonPropsDefine, ButtonGroup, ButtonGroupPropsDefine };
 export default Button;
