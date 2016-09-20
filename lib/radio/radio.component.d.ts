f4039a2316b793d2eaebb49cca234f24463d889d
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 15:45:26 +0800

    add tabs

--- lib/radio/radio.component.d.ts
+++ lib/radio/radio.component.d.ts
@@ -4,8 +4,12 @@ import './radio.scss';
 export default class Radio extends React.Component<typings.PropsDefine, typings.StateDefine> {
     static defaultProps: typings.PropsDefine;
     state: typings.StateDefine;
+
     constructor(props: typings.PropsDefine);
+
     componentWillReceiveProps(nextProps: typings.PropsDefine): void;
+
     handleChange(event: any): void;
+
     render(): JSX.Element;
 }
