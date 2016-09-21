23b9ee1ad41a50e15370d8d27bce5c8a9c6b592d
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 09:52:17 +0800

    开发

--- demo/basic.tsx
+++ demo/basic.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import {observer} from 'mobx-react'
-import TransparentlyProps from '../index'
 
 @observer
 export default class Demo extends React.Component <any, any> {
@@ -9,7 +8,7 @@ export default class Demo extends React.Component <any, any> {
 
     render() {
         return (
-            <TransparentlyProps />
+            <div>提供给所有组件透传 Props 使用，例如任何组件都可以接受 style className 等属性，让我们对组件的控制更加灵活</div>
         )
     }
 }
