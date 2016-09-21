4f8b9973d91d2dbcf97884efc217d08c2e86b7fe
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 09:56:34 +0800

    发布组件

--- lib/checkbox/checkbox.type.d.ts
+++ lib/checkbox/checkbox.type.d.ts
@@ -1,4 +1,5 @@
-export interface PropsDefine {
+import { TransparentlyPropsPropsDefine } from  'nt-transparently-props';
+export interface PropsDefine extends TransparentlyPropsPropsDefine {
     checked?: boolean;
     defaultChecked?: boolean;
     onChange?: (checked?: boolean) => void;
