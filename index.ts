5b3fff181a9c1ea31d3854fa0b3546aa6d8ee2e3
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 09:38:55 +0800

    开发

---
+++ index.ts
@@ -0,0 +1,6 @@
+import TransparentlyProps from './transparently-props/transparently-props.component'
+import {PropsDefine as TransparentlyPropsPropsDefine} from './transparently-props/transparently-props.type'
+
+export {TransparentlyProps, TransparentlyPropsPropsDefine}
+export default TransparentlyProps
+                
