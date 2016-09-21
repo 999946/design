f5438b0582eb59899e8e56fa2d22cdbab7fdc8bf
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 09:56:34 +0800

    发布组件

--- lib/collapse/collapse.type.d.ts
+++ lib/collapse/collapse.type.d.ts
@@ -1,4 +1,5 @@
-export interface PropsDefine  {
+import { TransparentlyPropsPropsDefine } from  'nt-transparently-props';
+export interface PropsDefine extends TransparentlyPropsPropsDefine {
     accordion?: boolean;
     onChange?: (key?: number | string) => void;
     value?: string | number | Array<number | string>;
