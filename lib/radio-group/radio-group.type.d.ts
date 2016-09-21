10173196aa06a23a897b0262f30df523b2de215a
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 09:56:34 +0800

    发布组件

--- lib/radio-group/radio-group.type.d.ts
+++ lib/radio-group/radio-group.type.d.ts
@@ -1,4 +1,5 @@
-export interface PropsDefine {
+import { TransparentlyPropsPropsDefine } from  'nt-transparently-props';
+export interface PropsDefine extends TransparentlyPropsPropsDefine {
     value?: any;
     defaultValue?: any;
     vertical?: boolean;
