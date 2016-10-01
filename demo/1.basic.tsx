7ca8829022bc8f9dd772f4a86d045c67b8548e29
huangziyi01 <huangziyi01@baidu.com>
2016-10-01 13:53:46 +0800

    新增透明度配置

--- demo/1.basic.tsx
+++ demo/1.basic.tsx
@@ -7,10 +7,14 @@ export default class Demo extends React.Component <any, any> {
     static title = '基本用法'
     static description = ``
 
+    handleChange(checked: boolean) {
+        console.log(checked)
+    }
+
     render() {
         return (
             <div>
-                <Checkbox/>
+                <Checkbox onChange={this.handleChange.bind(this)}/>
                 <Checkbox checked/>
                 <Checkbox>点击选中</Checkbox>
             </div>
