55575fbc409e60141daea70b10a6d38fb1e252cf
huangziyi01 <huangziyi01@baidu.com>
2016-10-10 15:03:03 +0800

    发布组件

---
+++ components/wefan/gaea-components/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var icon_component_1 = require('./icon/icon.component');
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = [icon_component_1.default];
