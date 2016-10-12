3c1160cae8432071abc9c1d165fbc3c71644283b
huangziyi01 <huangziyi01@baidu.com>
2016-10-12 15:13:52 +0800

    add line

--- line/line.type.ts
+++ line/line.type.ts
@@ -1,11 +1,39 @@
 import * as React from 'react'
+import {ViewProperties} from 'react-native'
+import {TransparentlyNativePropsPropsDefine} from '../../transparently-native-props/index'
 
-export interface PropsDefine {
+export interface PropsDefine extends ViewProperties, TransparentlyNativePropsPropsDefine {
+    /**
+     * 宽度
+     */
+    width?: number
 
+    /**
+     * 线条颜色
+     */
+    color?: string
+
+    /**
+     * 线条样式
+     */
+    lineType?: LineType
+
+    /**
+     * 是否垂直
+     */
+    vertical?: boolean
+
+    /**
+     * 样式
+     */
+    style?: React.ViewStyle
 }
 
 export class Props implements PropsDefine {
-
+    width = 0.5
+    color = '#E3E4E6'
+    lineType = 'solid' as LineType
+    vertical = false
 }
 
 export interface StateDefine {
@@ -15,4 +43,5 @@ export interface StateDefine {
 export class State implements StateDefine {
 
 }
-                
+
+export type LineType = 'solid' | 'dotted' | 'dashed'
