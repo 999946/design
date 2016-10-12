f37ac3dd7798bfff61ff052cf17001ef2eb542b2
huangziyi01 <huangziyi01@baidu.com>
2016-10-12 18:29:08 +0800

    add loading

---
+++ loading/loading.type.ts
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+export interface PropsDefine {
+    /**
+     * 样式
+     */
+    style?: React.ViewStyle
+}
+
+export class Props implements PropsDefine {
+
+}
+
+export interface StateDefine {
+
+}
+
+export class State implements StateDefine {
+
+}
+                
