891e87d6a75c45734518cd0c2fa15d563bba6662
huangziyi01 <huangziyi01@baidu.com>
2016-10-18 11:33:42 +0800

    发布组件

---
+++ components/web-common/auto-complete/lib/index.d.ts
@@ -0,0 +1,4 @@
+import AutoComplete from './auto-complete/auto-complete.component';
+import { PropsDefine as AutoCompletePropsDefine } from './auto-complete/auto-complete.type';
+export { AutoComplete, AutoCompletePropsDefine };
+export default AutoComplete;
