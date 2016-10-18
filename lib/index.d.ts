b699c44116f0c67abfe0fbdffbca3a7d17a8d32c
huangziyi01 <huangziyi01@baidu.com>
2016-10-18 14:43:59 +0800

    发布组件

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import Tooltip from './tooltip/tooltip.component';
+import { PropsDefine as TooltipPropsDefine } from './tooltip/tooltip.type';
+export { Tooltip, TooltipPropsDefine };
+export default Tooltip;
