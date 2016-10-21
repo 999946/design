ddb8c9805e59314060a14aa0ade475dc0b13051e
huangziyi01 <huangziyi01@baidu.com>
2016-10-21 10:56:09 +0800

    开发

---
+++ swiper/swiper.type.ts
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+export interface PropsDefine {
+
+}
+
+export class Props implements PropsDefine {
+
+}
+
+export interface StateDefine {
+
+}
+
+export class State implements StateDefine {
+
+}
+                
