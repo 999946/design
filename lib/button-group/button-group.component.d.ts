7bc733182ce3df09666185fea8032835687bff6f
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/button-group/button-group.component.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as typings from './button-group.type';
-import './button-group.scss';
-export default class ButtonGroup extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    render(): JSX.Element;
-}
