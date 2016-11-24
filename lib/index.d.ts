f446f8fc09ae3693bdfdb6e9df6fd372d8550050
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import TransparentlyNativeProps from './transparently-native-props/transparently-native-props.component';
-import { PropsDefine as TransparentlyNativePropsPropsDefine } from './transparently-native-props/transparently-native-props.type';
-export { TransparentlyNativeProps, TransparentlyNativePropsPropsDefine };
-export default TransparentlyNativeProps;
