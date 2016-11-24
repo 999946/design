5e0ebfbbfd5674ffa624e8999f195ce4c6e52445
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Radio from './radio/radio.component';
-import { PropsDefine as RadioPropsDefine } from './radio/radio.type';
-import RadioGroup from './radio-group/radio-group.component';
-import { PropsDefine as RadioGroupPropsDefine } from './radio-group/radio-group.type';
-export { Radio, RadioPropsDefine, RadioGroup, RadioGroupPropsDefine };
-export default Radio;
