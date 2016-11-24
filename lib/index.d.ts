bee37198d7d57a37cc7c2bd1534cc3330de33a8d
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Message from './message/message.component';
-import { PropsDefine as MessagePropsDefine } from './message/message.type';
-export { Message, MessagePropsDefine };
-export default Message;
