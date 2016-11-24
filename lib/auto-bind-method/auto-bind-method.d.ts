a4e220e94462f418e6f1302576d1ac6a0e3a4f8b
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/auto-bind-method/auto-bind-method.d.ts
+++
@@ -1,6 +0,0 @@
-declare var _default: <T extends Function>(target: any, key: string, descriptor: TypedPropertyDescriptor<T>) => {
-    configurable: boolean;
-    get(): any;
-    set(newValue: any): any;
-};
-export default _default;
