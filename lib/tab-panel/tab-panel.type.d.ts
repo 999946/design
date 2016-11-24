04e59030b7ed143c2dd85443e64e85ca1fc67c5f
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:48:58 +0800

    发布组件

---
+++ lib/tab-panel/tab-panel.type.d.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { TransparentlyPropsPropsDefine } from  'nt-transparently-props';
+export interface PropsDefine extends TransparentlyPropsPropsDefine {
+    activeKey?: string;
+    tab?: string;
+    tabRender?: (isActive?: boolean) => React.ReactElement<any>;
+    active?: boolean;
+    others?: any;
+}
+export declare class PropsGaea {
+    gaeaName: string;
+    gaeaIcon: string;
+    gaeaUniqueKey: string;
+}
+export declare class Props extends PropsGaea implements PropsDefine {
+    activeKey: string;
+    tab: string;
+    active: boolean;
+    tabRender: () => any;
+}
+export interface StateDefine {
+}
+export declare class State implements StateDefine {
+}
