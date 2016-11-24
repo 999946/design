cae25660bed33fe56d171ad444582f2367b25f26
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- test/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const image_component_1 = require('./image/image.component');
-exports.Image = image_component_1.default;
-Object.defineProperty(exports, "__esModule", {value: true});
-exports.default = image_component_1.default;
