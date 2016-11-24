2bbe61d9f3dff1d50550b66434b54dd97fb86c99
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Button from './button/button.component';
-import { PropsDefine as ButtonPropsDefine } from './button/button.type';
-export { Button, ButtonPropsDefine };
-export default Button;
