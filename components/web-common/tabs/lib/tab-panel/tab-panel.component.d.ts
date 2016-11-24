a1a08985076901b32efd7b7a658e0d851308560d
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:49:08 +0800

    发布组件

--- components/web-common/tabs/lib/tab-panel/tab-panel.component.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as typings from './tab-panel.type';
-import './tab-panel.scss';
-export default class TabPanel extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    render(): JSX.Element;
-}
