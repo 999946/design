f665b5fa71818dc5f46ebeb26825992e47579da6
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Loading from './loading/loading.component';
-import { PropsDefine as LoadingPropsDefine } from './loading/loading.type';
-export { Loading, LoadingPropsDefine };
-export default Loading;
