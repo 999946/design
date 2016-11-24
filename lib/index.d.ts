eaae101dc405b244188e4767277baa02dae8a429
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Swiper from './swiper/swiper.component';
-import { PropsDefine as SwiperPropsDefine } from './swiper/swiper.type';
-export { Swiper, SwiperPropsDefine };
-export default Swiper;
