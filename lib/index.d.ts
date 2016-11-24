0391ece3c5e0b3fdb835115e8c13db3b20a8e759
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Switch from './switch/switch.component';
-import { PropsDefine as SwitchPropsDefine } from './switch/switch.type';
-export { Switch, SwitchPropsDefine };
-export default Switch;
