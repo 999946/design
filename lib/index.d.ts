9e46cc2b62c51066da43a5cefea287232ac7d1ba
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Number from './number/number.component';
-import { PropsDefine as NumberPropsDefine } from './number/number.type';
-export { Number, NumberPropsDefine };
-export default Number;
