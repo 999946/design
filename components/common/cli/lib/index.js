51f6a574e99a1771303a1e7259de1faa087f1343
huangziyi01 <huangziyi01@baidu.com>
2016-11-27 20:49:29 +0800

    发布组件

---
+++ components/common/cli/lib/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var commander = require('commander');
+commander.version('1.0.0').option('-i, --init', '把项目初始化到能用的程度').parse(process.argv);
+if (commander['init']) {
+    console.log(__dirname);
+}
