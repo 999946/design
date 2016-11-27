290baa7608aa464a94f676ae5d1706d2627ce915
huangziyi01 <huangziyi01@baidu.com>
2016-11-27 21:08:59 +0800

    开发

--- index.ts
+++ index.ts
@@ -1,12 +1,35 @@
 #!/usr/bin/env node
 
 import * as path from 'path'
+import { execSync } from 'child_process'
 import * as commander from 'commander'
+
+/**
+ * 根据后缀找文件
+ */
+const getFilesBySuffix = (suffix: string, modulePath: string): Array<string> => {
+    let files = execSync(`find ${modulePath} -name "*.${suffix}"`).toString().split('\n')
+    files = files.filter((item) => {
+        return item !== ''
+    })
+    return files
+}
+
+/**
+ * 把引用还原
+ */
 
 commander.version('1.0.0')
     .option('-i, --init', '把项目初始化到能用的程度')
     .parse(process.argv)
 
 if (commander['init']) {
-    console.log(process.cwd())
+    // 找到当前目录下所有 ts,tsx 文件
+    const tsFilePaths = getFilesBySuffix('ts', process.cwd())
+    const tsxFilePaths = getFilesBySuffix('tsx', process.cwd())
+    const allTsFilePaths = tsFilePaths.concat(tsxFilePaths)
+
+    allTsFilePaths.forEach(tsFilePath => {
+        console.log(tsFilePath)
+    })
 }
