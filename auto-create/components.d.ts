39fe68f3b7e81388b4ea174f84e4ee6d5f19eed3
黄子毅 <huangziyi@huangziyideMacBook-Pro.local>
2016-12-05 21:30:45 +0800

    update

--- auto-create/components.d.ts
+++ auto-create/components.d.ts
@@ -1,280 +1,285 @@
 
-declare module 'nt-web-button' {
-    export interface Button { }
-    export interface ButtonPropsDefine { }
-    export interface ButtonGroup { }
-    export interface ButtonGroupPropsDefine { }
-
-}
-
-declare module 'nt-web-input' {
-    export interface Input { }
-    export interface InputPropsDefine { }
-
-}
-
-declare module 'nt-web-number' {
-    export interface Number { }
-    export interface NumberPropsDefine { }
-
-}
-
-declare module 'nt-web-auto-complete' {
-    export interface AutoComplete { }
-    export interface AutoCompletePropsDefine { }
-
-}
-
-declare module 'nt-web-switch' {
-    export interface Switch { }
-    export interface SwitchPropsDefine { }
-
-}
-
-declare module 'nt-web-radio' {
-    export interface Radio { }
-    export interface RadioPropsDefine { }
-    export interface RadioGroup { }
-    export interface RadioGroupPropsDefine { }
-
-}
-
-declare module 'nt-web-checkbox' {
-    export interface Checkbox { }
-    export interface CheckboxPropsDefine { }
-
-}
-
-declare module 'nt-web-select' {
-    export interface Select { }
-    export interface SelectPropsDefine { }
-    export interface Option { }
-    export interface OptionPropsDefine { }
-    export interface OptGroup { }
-    export interface OptGroupPropsDefine { }
-
-}
-
-declare module 'nt-web-collapse' {
-    export interface Collapse { }
-    export interface CollapsePropsDefine { }
-    export interface CollPanel { }
-    export interface CollPanelPropsDefine { }
-
-}
-
-declare module 'nt-web-tooltip' {
-    export interface Tooltip { }
-    export interface TooltipPropsDefine { }
-
-}
-
-declare module 'nt-web-message' {
-    export interface Message { }
-    export interface MessagePropsDefine { }
-
-}
-
-declare module 'nt-web-modal' {
-    export interface Modal { }
-    export interface ModalPropsDefine { }
-
-}
-
-declare module 'nt-web-tabs' {
-    export interface Tabs { }
-    export interface TabsPropsDefine { }
-    export interface TabPanel { }
-    export interface TabPanelPropsDefine { }
-
-}
-
-declare module 'nt-web-tree' {
-    export interface Tree { }
-    export interface TreePropsDefine { }
-    export interface TreeNode { }
-    export interface TreeNodePropsDefine { }
-
-}
-
-declare module 'nt-web-json-tree' {
-    export interface JsonTree { }
-    export interface JsonTreePropsDefine { }
-
-}
-
-declare module 'nt-web-margin-padding-editor' {
-    export interface MarginPaddingEditor { }
-    export interface MarginPaddingEditorPropsDefine { }
-
-}
-
-declare module 'nt-web-badge' {
-    export interface Badge { }
-    export interface BadgePropsDefine { }
-
-}
-
-declare module 'nt-web-loading' {
-    export interface Loading { }
-    export interface LoadingPropsDefine { }
-
-}
-
-declare module 'nt-web-timeago' {
-    export interface Timeago { }
-    export interface TimeagoPropsDefine { }
-
-}
-
-declare module 'nt-web-image-preload' {
-    export interface ImagePreload { }
-    export interface ImagePreloadPropsDefine { }
-
-}
-
-declare module 'nt-web-render-to' {
-    export interface RenderTo { }
-    export interface RenderToPropsDefine { }
-
-}
-
-declare module 'nt-button' {
-    export interface Button { }
-    export interface ButtonPropsDefine { }
-
-}
-
-declare module 'nt-line' {
-    export interface Line { }
-    export interface LinePropsDefine { }
-
-}
-
-declare module 'nt-image' {
-    export interface Image { }
-    export interface ImagePropsDefine { }
-
-}
-
-declare module 'nt-gif' {
-    export interface Gif { }
-    export interface GifPropsDefine { }
-
-}
-
-declare module 'nt-image-zoom' {
-    export interface ImageZoom { }
-    export interface ImageZoomPropsDefine { }
-
-}
-
-declare module 'nt-image-viewer' {
-    export interface ImageViewer { }
-    export interface ImageViewerPropsDefine { }
-
-}
-
-declare module 'nt-swiper' {
-    export interface Swiper { }
-    export interface SwiperPropsDefine { }
-
-}
-
-declare module 'nt-transmit-transparently' {
-    export interface TransmitTransparently { }
-    export interface TransmitTransparentlyPropsDefine { }
-    export interface others { }
-    export interface OthersPropsDefine { }
-
-}
-
-declare module 'nt-auto-bind' {
-    export interface autoBindMethod { }
-    export interface AutoBindMethodPropsDefine { }
-    export interface autoBindClass { }
-    export interface AutoBindClassPropsDefine { }
-
-}
-
-declare module 'nt-transparently-props' {
-    export interface TransparentlyProps { }
-    export interface TransparentlyPropsPropsDefine { }
-
-}
-
-declare module 'nt-transparently-native-props' {
-    export interface TransparentlyNativeProps { }
-    export interface TransparentlyNativePropsPropsDefine { }
-
-}
-
-declare module 'nt-inject-instance' {
-    export interface injectInstance { }
-    export interface inject { }
-
-}
-
-declare module 'nt-wefan-button' {
-    export interface Button { }
-    export interface ButtonPropsDefine { }
-
-}
-
-declare module 'nt-wefan-image' {
-    export interface Image { }
-    export interface ImagePropsDefine { }
-
-}
-
-declare module 'nt-wefan-resource-card' {
-    export interface ResourceCard { }
-    export interface ResourceCardPropsDefine { }
-
-}
-
-declare module 'nt-wefan-navbar' {
-    export interface Navbar { }
-    export interface NavbarPropsDefine { }
-
-}
-
-declare module 'nt-wefan-loading' {
-    export interface Loading { }
-    export interface LoadingPropsDefine { }
-
-}
-
-declare module 'nt-wefan-gaea-components' {
-
-}
-
-declare module 'nt-editor-gaea-editor' {
-    export interface GaeaEditor { }
-    export interface GaeaEditorPropsDefine { }
-
-}
-
-declare module 'nt-editor-gaea-preview' {
-    export interface GaeaPreview { }
-    export interface GaeaPreviewPropsDefine { }
-
-}
-
-declare module 'nt-editor-gaea-web-components' {
-    export interface Components { }
-
-}
-
-declare module 'nt-editor-gaea-native-components' {
-
-}
-
-declare module 'nt-editor-gaea-model' {
-
-}
-
-declare module 'nt-editor-gaea-helper' {
-    export interface gaeaHelper { }
-
-}
+                declare module 'nt-web-button' {    
+                    export interface Button {}
+export interface ButtonPropsDefine {}
+export interface ButtonGroup {}
+export interface ButtonGroupPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-input' {    
+                    export interface Input {}
+export interface InputPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-number' {    
+                    export interface Number {}
+export interface NumberPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-auto-complete' {    
+                    export interface AutoComplete {}
+export interface AutoCompletePropsDefine {}
+
+                }
+            
+                declare module 'nt-web-switch' {    
+                    export interface Switch {}
+export interface SwitchPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-radio' {    
+                    export interface Radio {}
+export interface RadioPropsDefine {}
+export interface RadioGroup {}
+export interface RadioGroupPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-checkbox' {    
+                    export interface Checkbox {}
+export interface CheckboxPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-select' {    
+                    export interface Select {}
+export interface SelectPropsDefine {}
+export interface Option {}
+export interface OptionPropsDefine {}
+export interface OptGroup {}
+export interface OptGroupPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-collapse' {    
+                    export interface Collapse {}
+export interface CollapsePropsDefine {}
+export interface CollPanel {}
+export interface CollPanelPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-tooltip' {    
+                    export interface Tooltip {}
+export interface TooltipPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-message' {    
+                    export interface Message {}
+export interface MessagePropsDefine {}
+
+                }
+            
+                declare module 'nt-web-modal' {    
+                    export interface Modal {}
+export interface ModalPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-tabs' {    
+                    export interface Tabs {}
+export interface TabsPropsDefine {}
+export interface TabPanel {}
+export interface TabPanelPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-tree' {    
+                    export interface Tree {}
+export interface TreePropsDefine {}
+export interface TreeNode {}
+export interface TreeNodePropsDefine {}
+
+                }
+            
+                declare module 'nt-web-json-tree' {    
+                    export interface JsonTree {}
+export interface JsonTreePropsDefine {}
+
+                }
+            
+                declare module 'nt-web-margin-padding-editor' {    
+                    export interface MarginPaddingEditor {}
+export interface MarginPaddingEditorPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-badge' {    
+                    export interface Badge {}
+export interface BadgePropsDefine {}
+
+                }
+            
+                declare module 'nt-web-loading' {    
+                    export interface Loading {}
+export interface LoadingPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-timeago' {    
+                    export interface Timeago {}
+export interface TimeagoPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-image-preload' {    
+                    export interface ImagePreload {}
+export interface ImagePreloadPropsDefine {}
+
+                }
+            
+                declare module 'nt-web-render-to' {    
+                    export interface RenderTo {}
+export interface RenderToPropsDefine {}
+
+                }
+            
+                declare module 'nt-button' {    
+                    export interface Button {}
+export interface ButtonPropsDefine {}
+
+                }
+            
+                declare module 'nt-line' {    
+                    export interface Line {}
+export interface LinePropsDefine {}
+
+                }
+            
+                declare module 'nt-image' {    
+                    export interface Image {}
+export interface ImagePropsDefine {}
+
+                }
+            
+                declare module 'nt-gif' {    
+                    export interface Gif {}
+export interface GifPropsDefine {}
+
+                }
+            
+                declare module 'nt-image-zoom' {    
+                    export interface ImageZoom {}
+export interface ImageZoomPropsDefine {}
+
+                }
+            
+                declare module 'nt-image-viewer' {    
+                    export interface ImageViewer {}
+export interface ImageViewerPropsDefine {}
+
+                }
+            
+                declare module 'nt-swiper' {    
+                    export interface Swiper {}
+export interface SwiperPropsDefine {}
+
+                }
+            
+                declare module 'nt-transmit-transparently' {    
+                    export interface TransmitTransparently {}
+export interface TransmitTransparentlyPropsDefine {}
+export interface others {}
+export interface OthersPropsDefine {}
+
+                }
+            
+                declare module 'nt-auto-bind' {    
+                    export interface autoBindMethod {}
+export interface AutoBindMethodPropsDefine {}
+export interface autoBindClass {}
+export interface AutoBindClassPropsDefine {}
+
+                }
+            
+                declare module 'nt-transparently-props' {    
+                    export interface TransparentlyProps {}
+export interface TransparentlyPropsPropsDefine {}
+
+                }
+            
+                declare module 'nt-transparently-native-props' {    
+                    export interface TransparentlyNativeProps {}
+export interface TransparentlyNativePropsPropsDefine {}
+
+                }
+            
+                declare module 'nt-inject-instance' {    
+                    export interface injectInstance {}
+export interface inject {}
+
+                }
+            
+                declare module 'nt-cli' {    
+                    
+                }
+            
+                declare module 'nt-wefan-button' {    
+                    export interface Button {}
+export interface ButtonPropsDefine {}
+
+                }
+            
+                declare module 'nt-wefan-image' {    
+                    export interface Image {}
+export interface ImagePropsDefine {}
+
+                }
+            
+                declare module 'nt-wefan-resource-card' {    
+                    export interface ResourceCard {}
+export interface ResourceCardPropsDefine {}
+
+                }
+            
+                declare module 'nt-wefan-navbar' {    
+                    export interface Navbar {}
+export interface NavbarPropsDefine {}
+
+                }
+            
+                declare module 'nt-wefan-loading' {    
+                    export interface Loading {}
+export interface LoadingPropsDefine {}
+
+                }
+            
+                declare module 'nt-wefan-gaea-components' {    
+                    
+                }
+            
+                declare module 'nt-editor-gaea-editor' {    
+                    export interface GaeaEditor {}
+export interface GaeaEditorPropsDefine {}
+
+                }
+            
+                declare module 'nt-editor-gaea-preview' {    
+                    export interface GaeaPreview {}
+export interface GaeaPreviewPropsDefine {}
+
+                }
+            
+                declare module 'nt-editor-gaea-web-components' {    
+                    export interface Components {}
+
+                }
+            
+                declare module 'nt-editor-gaea-native-components' {    
+                    
+                }
+            
+                declare module 'nt-editor-gaea-model' {    
+                    
+                }
+            
+                declare module 'nt-editor-gaea-helper' {    
+                    export interface gaeaHelper {}
+
+                }
+            
