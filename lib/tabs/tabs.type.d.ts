796dbfbd8aded26f0bd52dfa785bfd06a5b37f3b
huangziyi01 <huangziyi01@baidu.com>
2017-01-09 13:35:23 +0800

    发布组件

---
+++ lib/tabs/tabs.type.d.ts
@@ -0,0 +1,28 @@
+import { TransparentlyPropsPropsDefine } from  'nt-transparently-props';
+export interface PropsDefine extends TransparentlyPropsPropsDefine {
+    defaultActiveKey?: string | number;
+    activeKey?: string | number;
+    onChange?: (key?: string | number) => void;
+    type?: Type;
+    others?: any;
+}
+export declare class PropsGaea {
+    gaeaName: string;
+    gaeaIcon: string;
+    gaeaUniqueKey: string;
+}
+export declare class Props extends PropsGaea implements PropsDefine {
+    defaultActiveKey: any;
+    activeKey: any;
+    onChange: () => void;
+}
+export declare type Type = 'retro';
+export interface StateDefine {
+    activeKey?: string | number;
+    moveBarStyle?: any;
+    isForward?: boolean;
+}
+export declare class State implements StateDefine {
+    moveBarStyle: {};
+    isForward: boolean;
+}
