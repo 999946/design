d0d045c3777b20398e0555cd6c1f03a2a5d724b4
huangziyi01 <huangziyi01@baidu.com>
2017-01-13 18:15:33 +0800

    update

--- utils/handle-browser-call.ts
+++ utils/handle-browser-call.ts
@@ -1,6 +1,7 @@
 import {browserHistory} from './provider'
 
 export default (functionName: string, param: any)=> {
+    console.log(functionName)
     // 响应浏览器的事件
     switch (functionName) {
         case 'back':
